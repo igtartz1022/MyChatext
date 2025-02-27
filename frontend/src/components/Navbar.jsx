5c30d7b78460a4fed9c0abe63d8f976244b988f5
igtartz1022 <tartz.glenn@gmail.com>
2025-02-27 17:23:05 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useAuthStore } from "../store/auth.store";
 import { Link, useNavigate } from "react-router-dom";
-import { IoChatbubbleOutline } from "react-icons/io";
-import { TbLogout2 } from "react-icons/tb";
-import { IoHome } from "react-icons/io";
+// import { IoChatbubbleOutline } from "react-icons/io";
+// import { TbLogout2 } from "react-icons/tb";
+// import { IoHome } from "react-icons/io";
 import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
@@ -24,13 +24,13 @@ const Navbar = () => {
           href="#"
           style={{ width: "100px" }}
         >
-          <IoHome />
+        
         </Link>
         <Link to="/chat" className="btn btn-outline-primary ">
-          Chat <IoChatbubbleOutline />
+          Chat 
         </Link>
         <button className="btn btn-outline-danger" onClick={handleLogout}>
-          <TbLogout2 /> Log Out
+           Log Out
         </button>
       </nav>
     </div>
